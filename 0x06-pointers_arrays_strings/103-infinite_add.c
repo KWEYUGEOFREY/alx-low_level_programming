06d0a3958aa4432259d3bc1eef9127c292be9261
GEOFREY KWEYU <38045476+KWEYUGEOFREY@users.noreply.github.com>
2021-09-26 01:19:34 +0300

    a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,52 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *infinite_add - add two numbers.
- *
- *@n1: first number.
- *@n2: second number.
- *@r: result.
- *@size_r: result size.
- *Return: the addition of n1 and n2.
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to be printed from the buffer.
  */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+void print_buffer(char *b, int size)
 {
-	int add = 0, len1, len2, i, j;
+	int byte, index;
 
-	for (len1 = 0; n1[len1]; len1++)
-	;
-	for (len2 = 0; n2[len2]; len2++)
-	;
-	if (len1 > size_r || len2 > size_r)
-		return (0);
-	len1--;
-	len2--;
-	size_r--;
-	for (i = 0; i < size_r; i++, len1--, len2--)
+	for (byte = 0; byte < size; byte += 10)
 	{
-		if (len1 >= 0)
-			add += n1[len1] - '0';
-		if (len2 >= 0)
-			add += n2[len2] - '0';
-		if (len1 < 0 && len2 < 0 && add == 0)
-			break;
-		r[i] = add % 10 + '0';
-		add /= 10;
+		printf("%08x: ", byte);
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				printf("  ");
+
+			else
+				printf("%02x", *(b + index + byte));
+
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
+		}
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
+			else
+				printf(".");
+		}
+
+		if (byte >= size)
+			continue;
+
+		printf("\n");
 	}
-	r[i] = '\0';
-	if (len1 >= 0 || len2 >= 0 || add)
-		return (0);
-	for (i--, j = 0; i > j; i--, j++)
-	{
-		add = r[i];
-		r[i] = r[j];
-		r[j] = add;
-	}
-	return (r);
+
+	if (size <= 0)
+		printf("\n");
 }
